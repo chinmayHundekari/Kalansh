272d32fbc9bd54008458b29ab3aa72ed0c666893
Chinmay Hundekari <chinmay.hundekari@gmail.com>
2016-10-20 23:50:36 +0530

    Correcting gitignore file and title input field

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,32 @@
-document.getElementById("editable").addEventListener("input", function() {
-   }, false);
+var editable_title = $('#editable_title')
+
+editable_title.on('input', function() {
+  return filter_newlines(editable_title);
+});
+
+
+function filter_newlines(div) {
+    var node, prev, _i, _len, _ref, _results;
+    prev = null;
+    _ref = div.contents();
+    _results = [];
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      node = _ref[_i];
+      if (node.nodeType === 3) {
+        node.nodeValue = node.nodeValue.replace('\n', '');
+        if (prev) {
+          node.nodeValue = prev.nodeValue + node.nodeValue;
+          $(prev).remove();
+        }
+        _results.push(prev = node);
+      } else if (node.tagName.toLowerCase() === 'br') {
+        _results.push($(node).remove());
+      } else {
+        $(node).css('display', 'inline');
+        filter_newlines($(node));
+        _results.push(prev = null);
+      }
+    }
+    return _results;
+  }
+
